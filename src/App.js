bf4851fea9880d0b01e59cf1ca6a4d01eaf93618
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-15 23:33:51 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Blogs from "./components/Blog";
-import  Pagination from "./components/Pagination";
+import Pagination from "./components/Pagination";
 
 export default function App() {
   return (
