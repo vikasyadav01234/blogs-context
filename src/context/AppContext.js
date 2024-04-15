844e0db7805bf74a99587efc1699e4741cf76d19
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-15 22:48:41 +0530

    main

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,7 +2,6 @@ import { createContext, useState } from "react";
 
 
 export const AppContext = createContext();
-
 function AppContextProvider({children}){
     const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
