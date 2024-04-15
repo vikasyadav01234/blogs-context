8ae35cebcaa67918ad035c59b2079cf0135bd769
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-15 22:25:51 +0530

    main

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AppContext } from "./context/AppContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <AppContextProvider>
     <App />
-  </React.StrictMode>
+  </AppContextProvider>
 );
