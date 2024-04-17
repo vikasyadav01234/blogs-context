7fe779316f15d137697d1cc066dff0f5c4fe7402
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-17 23:30:58 +0530

    main

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import {AppContext} from '../context/AppContext'
+import {AppContext} from '../context/AppContext';
 import Spinner from "./Spinner";
 import "./Blog.css";
 const Blogs = () => {
