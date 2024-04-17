6bc5030982ba429713dca6bba59181f1786c4496
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-17 23:24:40 +0530

    main

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,7 +5,7 @@ export const Pagination = () => {
     const {page, handlePageChange, totalPages} =useContext(AppContext)
     
   return (
-    <div className='w-full flex justify-center items-center border-2'>
+    <div className='w-full flex justify-center items-center border-2 fixed bottom-0 bg-white'>
       <div className='flex justify-between w-11/12 max-w-[670px] py-2'>
         <div className='flex gap-x-2'>
         { page >1 &&
