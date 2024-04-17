42ce762ff83c1ac298b8078c246c4ca67a178e94
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-17 23:31:15 +0530

    main

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -11,6 +11,7 @@ const Blogs = () => {
     return (
         
         <div className="w-11/12 max-w-[670px] h-screen py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] justify-center items-center">
+      
             {
                 loading ? (<Spinner/>) : (
                     posts.length === 0 ? (<div>
