c773d2c769f97d653806fde38c928dfefe69ca7c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-17 19:23:31 +0530

    main

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Header from "./components/Header";
 import Blogs from "./components/Blog";
 import Pagination from "./components/Pagination";
 import { useContext } from "react";
+import { AppContext } from "./context/AppContext";
 
 export default function App() {
-  const {fetchBlogPosts} = useContext
+  const {fetchBlogPosts} = useContext(AppContext)
   return (
     <div>
       <Header/>
