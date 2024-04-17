eba5e4dff290b990e5cfd0c1b385114e70b91f2d
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-17 22:40:30 +0530

    main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <div className='border shadow-md py-2'>
+    <div className='w-full border shadow-md py-2'>
         <header className='text-center'>
             <h1 className='text-2xl font-bold uppercase'>Code Blogs</h1>
         </header>
