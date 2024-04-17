d67502262c885869c66c4881bc09b09fd831876b
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-17 22:55:16 +0530

    main

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -21,7 +21,7 @@ const Blogs = () => {
                             <p className="text-[10px]">
                                 By <span className="italic">{post.author}</span> on <span className="underline font-bold">{post.category}</span>
                             </p>
-                            <p>Posted on {post.date}</p>
+                            <p className="text-[10px]">Posted on {post.date}</p>
                             <p>{post.content}</p>
                             <div>
                                 {post.tags.map((tag, index) => {
