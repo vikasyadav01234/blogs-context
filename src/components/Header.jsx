22768e28ded60456c3cdc08564570c62d09c0cae
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-17 23:02:16 +0530

    main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div className='w-full border shadow-md py-2'>
         <header className='text-center'>
-            <h1 className='text-2xl font-bold uppercase'>Code Blogs</h1>
+            <h1 className='text-3xl font-bold uppercase'>Code Blogs</h1>
         </header>
     </div>
   )
