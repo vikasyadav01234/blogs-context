555858cdce2b17e05990fbaedba6a9b11f8b541e
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 11:59:11 +0530

    main

---
+++ src/Pages/TagPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const TagPage = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default TagPage
