1370d4e9d547d497431a18a8475444a5157ffd67
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 12:22:06 +0530

    main

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const BlogPage = () => {
     const [blog, setBlog] = useState(null);
+    const [relatedBlogs, setRelatedBlogs] = useState([]);
   return (
     
   )
