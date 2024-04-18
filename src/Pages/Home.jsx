cd281153066a330f48058179c727ac553b774188
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 11:57:12 +0530

    main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
+import Blogs from '../components/Blog'
+import Pagination from '../components/Pagination'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <div>
+            <Blogs/>
+            
+            <Pagination/>
+        </div>
+
     </div>
   )
 }
