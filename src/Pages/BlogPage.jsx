598c91e003dfadcd45eab26ab642defbf4ee55dd
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 12:25:25 +0530

    main

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useLocation, useNavigation } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
 
 const BlogPage = () => {
     const [blog, setBlog] = useState(null);
     const [relatedBlogs, setRelatedBlogs] = useState([]);
     const location = useLocation();
     const navigation = useNavigation();
+    const {setLoading, loading} = useContext(AppContext);
   return (
     <div>
-        
+
     </div>
   )
 }
