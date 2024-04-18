8b64a54f7e21919c222b2d4ef33bec126e10be6a
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 12:23:15 +0530

    main

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+import { useLocation, useNavigation } from 'react-router-dom';
 
 const BlogPage = () => {
     const [blog, setBlog] = useState(null);
     const [relatedBlogs, setRelatedBlogs] = useState([]);
+    const location = useLocation();
+    const navigation = useNavigation();
   return (
     
   )
