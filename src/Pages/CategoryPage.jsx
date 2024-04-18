9154f42b1297ef1658f40d7f11e36d46fd8c987d
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 12:08:18 +0530

    main

---
+++ src/Pages/CategoryPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const CategoryPage = () => {
+  return (
+    <div>
+        <Header/>
+        <div>
+            <button
+            onClick={() => }
+        </div>
+    </div>
+  )
+}
+
+export default CategoryPage
