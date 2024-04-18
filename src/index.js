adf857f05c9d7010ab521c4a140653fc74f7d36b
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 10:37:58 +0530

    main

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import AppContextProvider from "./context/AppContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AppContextProvider>
-    <App />
-  </AppContextProvider>
+  <BrowseRouter>
+    
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowseRouter>
 );
