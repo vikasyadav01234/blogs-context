e4dc254f7be07719417c5164ce32764287f0ed34
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 12:09:10 +0530

    main

--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Header from '../components/Header'
+import { useLocation, useNavigation } from 'react-router-dom'
 
 const CategoryPage = () => {
+    const navigation = useNavigation();
+    const location = useLocation
   return (
     <div>
         <Header/>
