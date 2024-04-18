40ede3f2eeb7bf5c83ea747bbf5bc787e087118e
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-18 12:01:14 +0530

    main

--- src/Pages/TagPage.jsx
+++ src/Pages/TagPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Header from '../components/Header'
+import { useNavigation } from 'react-router-dom'
 
 const TagPage = () => {
+    const navigation = useNavigation
   return (
     <div>
         <Header/>
+        <div>
+            <button
+            onClick={()=> }
+            >
+                back
+            </button>
+        </div>
     </div>
   )
 }
