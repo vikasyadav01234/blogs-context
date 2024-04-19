cadca166fcb9632eb8457919889b4b8e87a6803b
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 09:47:16 +0530

    main

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -17,19 +17,7 @@ const Blogs = () => {
                     posts.length === 0 ? (<div>
                         <p>No Post Found</p>
                     </div>) : (posts.map( (post) => (
-                        <div key={post.id}>
-                            <p className="font-bold text-lg">{post.title}</p>
-                            <p className="text-sm mt-[4px]">
-                                By <span className="italic">{post.author}</span> on <span className="underline font-bold">{post.category}</span>
-                            </p>
-                            <p className="text-md mt-[4px]">Posted on {post.date}</p>
-                            <p className="text-sm mt-[14px]">{post.content}</p>
-                            <div className="flex gap-x-3">
-                                {post.tags.map((tag, index) => {
-                                    return <span key={index} className="text-blue-500 underline font-bold text-xs mt-[4px]">{`#${tag}`}</span>
-                                })}
-                            </div>
-                        </div>
+                        <BlogDetails key={post.id} post={post}/>
                     )))
                 )
             }
