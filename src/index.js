a1078b827923ac4ccdcafea462af3621da4c1642
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 10:56:31 +0530

    main

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+      <AppContextProvider>
+        <App />
+        
+      </AppContextProvider>
   </BrowserRouter>
+
 );
