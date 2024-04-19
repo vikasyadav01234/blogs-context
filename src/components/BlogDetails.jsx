cc74683c10449f9310d5eecc830de634c029139f
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 10:30:03 +0530

    main

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -3,29 +3,27 @@ import { NavLink } from 'react-router-dom'
 
 const BlogDetails = ({post}) => {
   return (
-    <div>
-        <NavLink to={`/blog/${post.id}`}>
-            <span>{post.title}</span>
+    <div className='mt-[50px]'>
+      <NavLink to={`/blog/${post.id}`} >
+        <span>{post.title}</span>
+      </NavLink>
+      <p>
+        By
+        <span>{post.author}</span>
+        on {" "}
+        <NavLink to={`/categories/${post.category.replaceAll(" ","-")}`}>
+            <span>{post.category}</span>
         </NavLink>
-        <p>
-            By
-            <span>{post.author}</span>
-            on {" "}
-            <NavLink to={`/categories/${post.category.replaceAll(" ","-")}`}>
-                <span>{post.category}</span>
+      </p>
+      <p> Posted on {post.date} </p>
+      <p> {post.content}</p>
+      <div>
+        {post.tags.map( (tag, index) => (
+            <NavLink key={index} to={`/tags/${tag.replaceAll(" ","-")}`}>
+                <span>{`#${tag}`}</span>
             </NavLink>
-        </p>
-        <p>Posted on {post.date}</p>
-        <p>{post.content}</p>
-        <div>
-            {
-                post.tag.map((tag, index) => (
-                    <NavLink key={index} to={`/tag/${tag.replaceAll(" ","-")}`}>
-                        <span>{`#${tag}`}</span>
-                    </NavLink>
-                ))
-            }
-        </div>
+        ) )}
+      </div>
     </div>
   )
 }
