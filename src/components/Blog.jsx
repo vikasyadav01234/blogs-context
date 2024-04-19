6ddf0369a7406c08db120a5ad8e3d5977cb18a01
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 10:32:35 +0530

    main

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,28 +1,25 @@
-import React, { useContext } from "react";
-import {AppContext} from '../context/AppContext';
-import Spinner from "./Spinner";
-import "./Blog.css";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 import BlogDetails from "./BlogDetails";
-const Blogs = () => {
-    //cansume
-    
-    const {posts, loading} = useContext(AppContext);
-    console.log("Printing inside Blog ");
-    console.log(posts);
-    return (
-        
-        <div className="w-11/12 max-w-[670px] h-screen py-8 flex flex-col gap-y-7 mt-[190px] mb-[190px] justify-center items-center">
-      
-            {
-                loading ? (<Spinner/>) : (
-                    posts.length === 0 ? (<div>
-                        <p>No Post Found</p>
-                    </div>) : (posts.map( (post) => (
-                        <BlogDetails key={post.id} post={post}/>
-                    )))
-                )
-            }
+
+export default function Blogs() {
+  const { posts, loading } = useContext(AppContext);
+
+  return (
+    <div className="flex flex-col gap-y-10 my-4">
+      {loading ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">Loading</p>
         </div>
-    )
+      ) : posts.length === 0 ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">No Blogs Found !</p>
+        </div>
+      ) : (
+        posts.map((post) => (
+          <BlogDetails key={post.id} post={post}/>
+        ))
+      )}
+    </div>
+  );
 }
-export default Blogs
