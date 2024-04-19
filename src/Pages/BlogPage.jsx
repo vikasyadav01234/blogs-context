005b70eb635519c419582f2378d55b90b096ccb8
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 10:09:55 +0530

    main

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -4,11 +4,12 @@ import { AppContext } from '../context/AppContext';
 import Header from "../components/Header"
 import BlogDetails from '../components/BlogDetails';
 import { baseUrl } from '../baseUrl';
-const BlogPage = () => {
+const BlogPage = ({post}) => {
     const [blog, setBlog] = useState(null);
     const [relatedBlogs, setRelatedBlogs] = useState([]);
     const location = useLocation();
     const navigation = useNavigation();
+
     const {setLoading, loading} = useContext(AppContext);
 
     async function fetchRelatedBlogs(){
