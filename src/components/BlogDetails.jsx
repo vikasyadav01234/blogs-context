abecce1d8c5c80c43a81a9c33809dbea279d7c5f
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 09:49:42 +0530

    main

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const BlogDetails = () => {
   return (
     <div>
-        
+        <NavLink to={`/blog/${post.id}`}/>
     </div>
   )
 }
