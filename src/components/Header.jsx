aa0f67ce6d17fff041f129f351c4282bd62e74b9
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 11:20:25 +0530

    main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-export default function Header() {
+import React from 'react'
+
+const Header = () => {
   return (
-    <header className="py-4 border-b-2 border-b-gray-300 drop-shadow-md fixed top-0 inset-x-0 bg-white">
-      <h1 className="font-bold text-3xl uppercase text-center">
-        Codehelp Blogs
-      </h1>
-    </header>
-  );
+    <div className='w-full border shadow-md py-4 fixed top-0 bg-white '>
+      <header className='text-center' >
+        <h1 className='text-3xl font-bold uppercase'>Codehelp Blogs</h1>
+      </header>
+    </div>
+  )
 }
+
+export default Header
