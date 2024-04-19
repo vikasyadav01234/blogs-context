2d385c218ab05b2dbe4723ddf4073614dca60364
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-19 10:49:47 +0530

    main

--- src/Pages/TagPage.jsx
+++ src/Pages/TagPage.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
-import { useLocation, useNavigation } from 'react-router-dom'
-import Blogs from '../components/Blog';
 import Pagination from '../components/Pagination';
+import Blogs from '../components/Blog';
+
 
 const TagPage = () => {
-    const navigation = useNavigation();
+
+    const navigation = useNavigate();
     const location = useLocation();
     const tag = location.pathname.split("/").at(-1);
   return (
     <div>
         <Header/>
         <div>
-            <button
-            onClick={()=> navigation(-1)}
+            <button 
+            onClick={() => navigation(-1)}
             >
-                Back
+                back
             </button>
             <h2>
                 Blogs Tagged <span>#{tag}</span>
             </h2>
-
         </div>
         <Blogs/>
         <Pagination/>
+      
     </div>
   )
 }
